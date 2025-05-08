165f14ceb75f38a40fd8c81447004f1e50ba150c
Dell <singh.pankaj@jarvis.consulting>
2025-05-08 14:25:31 +0530

    added

--- Project1/snake.js
+++ Project1/snake.js
@@ -8,4 +8,64 @@ menuOpenButton.addEventListener("click",()=>{
 })
 
 
-menuCloseButton.addEventListener("click",()=>menuOpenButton.click())
+menuCloseButton.addEventListener("click",()=>menuOpenButton.click());
+
+
+// Iniial Swiper
+const swiper = new Swiper('.slider-wrapper', {
+   
+    loop: true,
+    // effect:'cards',
+    // cardEffect:{
+    //     slideShows:true,
+    //     rotate:false,
+    //     perSlideOffset:30,
+    //     perSlideRotate:2,
+    // },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // Keyboard controls
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+
+    // Mouse controls
+    mousewheel: {
+        invert: true,
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+
+    // Responsive Breakpoints
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+            effect:'card',
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3
+        }
+    },
+    
+ 
+  
+    
+  });
